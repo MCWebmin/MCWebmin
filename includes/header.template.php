bc1ad7156d6f05d1d906263e121005441fb85ecd
Matthew Hall <matt@kerplunc.com>
2010-12-20 01:35:10 -0700

    Various Updates

--- includes/header.template.php
+++ includes/header.template.php
@@ -1,10 +1,8 @@
 <html>
 <head>
 <title>MCWebmin</title>
-<?php
-includes "config.php";
-includes "functions.php";
-?>
+<?php include "config.php"; ?>
+<?php include "functions.php"; ?>
 <body background="http://www.minecraft.net/img/bg.gif">
 <link rel="stylesheet" href="../includes/style.css" type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
