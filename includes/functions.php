32845fd0ab0e52c306d673f14ce08196921c8ee4
Matthew Hall <matt@kerplunc.com>
2010-12-20 01:39:02 -0700

    Updated Functions

--- includes/functions.php
+++ includes/functions.php
@@ -2,26 +2,27 @@
 function startServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft start");
         echo $output;
-        echo "<br>";
+        echo "<br />";
 }
 
 function stopServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft stop");
         echo $output;
-        echo "<br>";
+        echo "<br />";
 }
 
 function upgrade(){
-    if(copy('http://www.minecraft.net/download/minecraft_server.jar', '../server_files/update/minecraft_server.jar')){
-        copy('../server_files/minecraft-server/minecraft_server.jar','../server_files/minecraft-server/minecraft_server.jar.bak');
-        stopServer();
-        copy('../server_files/update/minecraft_server.jar', '../server_files/minecraft-server/minecraft_server.jar');
-        startServer();
-        echo "Upgrade succesfull <br>";
-    } else {
-        return false;
-    }
+    $output = shell_exec("sudo /etc/init.d/minecraft upgrade");
+        echo $output;
+        echo "<br />";
 }
+
+function status(){
+    $output = shell_exec("sudo /etc/init.d/minecraft status");
+        echo $output;
+        echo "<br />";
+}
+
 
 
 ?>
