9c08c820c9d58ea241fff3704296aa4359ef6b2a
Matthew Hall <matt@kerplunc.com>
2010-12-21 01:58:20 -0700

    sudo fixes

--- www/run.php
+++ www/run.php
@@ -3,38 +3,38 @@ if (isset($_GET['run'])) $linkchoice=$_GET['run'];
 switch($linkchoice){
 
 case 'start' :
-    $output = shell_exec("sudo -u mcwebmin /etc/init.d/minecraft start");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft start");
 	echo $output;
 	echo "$output";
 	echo "<br />";
     break;
 
 case 'stop' :
-    $output = shell_exec("sudo -u mcwebmin /etc/init.d/minecraft stop");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft stop");
 	echo $output;
 	echo "<br />";
     break;
 
 case 'update' :
-    $output = shell_exec("sudo -u mcwebmin /etc/init.d/minecraft update");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft update");
 	echo $output;
 	echo "<br />";
     break;
 
 case 'restart' : 
-    $output = shell_exec("sudo -u mcwebmin /etc/init.d/minecraft restart");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft restart");
 	echo $output;
 	echo "<br />";
     break;
 
 case 'status' :
-    $output = shell_exec("sudo -u mcwebmin /etc/init.d/minecraft status");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft status");
 	echo $output;
 	echo "<br />";
     break; 
      
 case 'backup' :
-    $output = shell_exec("sudo -u mcwebmin /etc/init.d/minecraft backup");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft backup");
 	echo $output;
 	echo "<br />";
     break;      
