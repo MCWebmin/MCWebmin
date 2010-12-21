dd23e20e083404d95845772d94aba839bc24c243
Matthew Hall <matt@kerplunc.com>
2010-12-21 00:36:55 -0700

    Added greybox code

--- includes/header.template.php
+++ includes/header.template.php
@@ -1,13 +1,32 @@
 <html>
 <head>
 <title>MCWebmin</title>
+<-- Include PHP Files -->
 <?php 
 include "config.php";
 include "functions.php";
 includeonce "rcon.class.php"; 
 ?>
+<-- Background -->
 <body background="http://www.minecraft.net/img/bg.gif">
-<link rel="stylesheet" href="../includes/style.css" type="text/css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
 
+<-- CSS Stylesheets -->
+<link rel="stylesheet" href="css/style.css" type="text/css">
+<link rel="stylesheet" href="css/greybox.css" type="text/css" media="all" />
+
+<!-- Javascript -->
+<script type="text/javascript" src="greybox.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+    <script type="text/javascript">
+      var GB_ANIMATION = true;
+      $(document).ready(function(){
+        $("a.greybox").click(function(){
+          var t = this.title || $(this).text() || this.href;
+          GB_show(t,this.href,470,600);
+          return false;
+        });
+      });
+    </script>
+	
+<!-- Header Image -->
 <center><img src="http://www.minecraft.net/img/logo_small.gif" alt="minecraft"></center>
--- www/index.php
+++ www/index.php
@@ -6,14 +6,14 @@
 <tr><td><?php echo $ip ?></font></td><td bgcolor=\"red\">Status</td><td><?php echo $port ?></td></tr>
 
 <tr>
-	<td><a href="?run=start">START</a></td>
-	<td><a href="?run=stop">STOP</a></td>
-	<td><a href="?run=update">UPDATE</a></td>
+	<td><a href="?run=start" class="greybox">START</a></td>
+	<td><a href="?run=stop" class="greybox">STOP</a></td>
+	<td><a href="?run=update" class="greybox">UPDATE</a></td>
 <tr>
 <tr>
-	<td><a href="?run=backup">BACKUP</a></td>
-	<td><a href="?run=restart">RESTART</a></td>
-	<td><a href="?run=status">STATUS</a></td>
+	<td><a href="?run=backup" class="greybox">BACKUP</a></td>
+	<td><a href="?run=restart" class="greybox">RESTART</a></td>
+	<td><a href="?run=status" class="greybox">STATUS</a></td>
 <tr>
 </table>
 <?php include "../includes/footer.template.php"
