5b8b0aae48cd3c1296a26426eedf9b01aec2031d
Matthew Hall <matt@kerplunc.com>
2010-12-21 00:58:07 -0700

    Updated functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -6,64 +6,62 @@ switch($linkchoice){
 
 case 'start' :
     startServer();
+	echo $output;
+	echo "<br />";
     break;
 
 case 'stop' :
     stopServer();
+	echo $output;
+	echo "<br />";
     break;
 
 case 'update' :
     updateServer();
+	echo $output;
+	echo "<br />";
     break;
 
 case 'restart' :
     restartServer();
+	echo $output;
+	echo "<br />";
     break;
 
 case 'status' :
     statusServer();
+	echo $output;
+	echo "<br />";
     break; 
      
 case 'backup' :
     backupServer();
+	echo $output;
+	echo "<br />";
     break;      
 }
 
 function startServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft start");
-        echo $output;
-        echo "<br />";
 }
 
 function stopServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft stop");
-        echo $output;
-        echo "<br />";
 }
 
 function updateServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft update");
-        echo $output;
-        echo "<br />";
 }
 
 function restartServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft restart");
-        echo $output;
-        echo "<br />";
 }
 
 function statusServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft status");
-        echo $output;
-        echo "<br />";
 }
 
 function backupServer(){
     $output = shell_exec("sudo /etc/init.d/minecraft backup");
-        echo $output;
-        echo "<br />";
 }
-
-
 ?>
