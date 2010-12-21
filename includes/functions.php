637a75379299b38d6c4287c9ed1ad935d4f27f0f
Matthew Hall <matt@kerplunc.com>
2010-12-21 03:38:03 -0700

    removing some unecessary functions.php stuff and now it's time to sleep

--- includes/functions.php
+++ includes/functions.php
@@ -1,37 +1,31 @@
 <?php
 function startServer(){
     $output = shell_exec("/etc/init.d/minecraft start");
-	echo "Starting Server";
 	echo "$output";
 }
 
 function stopServer(){
     $output = shell_exec("/etc/init.d/minecraft stop");
-	echo "Stopping Server";
 	echo "$output";
 }
 
 function updateServer(){
     $output = shell_exec("/etc/init.d/minecraft update");
-	echo "Updating Server";
 	echo "$output";
 }
 
 function restartServer(){
     $output = shell_exec("/etc/init.d/minecraft restart");
-	echo "Restarting Server";
 	echo "$output";
 }
 
 function statusServer(){
     $output = shell_exec("/etc/init.d/minecraft status");
-	echo "Server Status";
 	echo "$output";
 }
 
 function backupServer(){
     $output = shell_exec("/etc/init.d/minecraft backup");
-	echo "Backing Up Server";
 	echo "$output";
 }
 ?>
