1a7a0f335960c9d985000260ff53cd69e3ca7a85
Matthew Hall <matt@kerplunc.com>
2010-12-21 00:20:26 -0700

    Included rcon class

--- includes/header.template.php
+++ includes/header.template.php
@@ -1,8 +1,11 @@
 <html>
 <head>
 <title>MCWebmin</title>
-<?php include "config.php"; ?>
-<?php include "functions.php"; ?>
+<?php 
+include "config.php";
+include "functions.php";
+includeonce "rcon.class.php"; 
+?>
 <body background="http://www.minecraft.net/img/bg.gif">
 <link rel="stylesheet" href="../includes/style.css" type="text/css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
