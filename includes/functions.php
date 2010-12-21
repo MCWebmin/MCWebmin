ae4a42c4bf9fae24db21e6f9d0599b263bd3da51
Matthew Hall <matt@kerplunc.com>
2010-12-21 03:35:53 -0700

    Apparently it wnats to have NO sudo

--- includes/functions.php
+++ includes/functions.php
@@ -1,36 +1,36 @@
 <?php
 function startServer(){
-    $output = shell_exec("/usr/bin/sudo -u mcwebmin -u mcwebmin /etc/init.d/minecraft start");
+    $output = shell_exec("/etc/init.d/minecraft start");
 	echo "Starting Server";
 	echo "$output";
 }
 
 function stopServer(){
-    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft stop");
+    $output = shell_exec("/etc/init.d/minecraft stop");
 	echo "Stopping Server";
 	echo "$output";
 }
 
 function updateServer(){
-    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft update");
+    $output = shell_exec("/etc/init.d/minecraft update");
 	echo "Updating Server";
 	echo "$output";
 }
 
 function restartServer(){
-    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft restart");
+    $output = shell_exec("/etc/init.d/minecraft restart");
 	echo "Restarting Server";
 	echo "$output";
 }
 
 function statusServer(){
-    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft status");
+    $output = shell_exec("/etc/init.d/minecraft status");
 	echo "Server Status";
 	echo "$output";
 }
 
 function backupServer(){
-    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft backup");
+    $output = shell_exec("/etc/init.d/minecraft backup");
 	echo "Backing Up Server";
 	echo "$output";
 }
