947cf47dfd13ca42df787d2f7b62b428d3c2bb96
Matthew Hall <matt@kerplunc.com>
2010-12-21 03:16:53 -0700

    full which links for sudo

--- includes/functions.php
+++ includes/functions.php
@@ -1,31 +1,31 @@
 <?php
 function startServer(){
-    $output = shell_exec("sudo /etc/init.d/minecraft start");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin -u mcwebmin /etc/init.d/minecraft start");
 	echo "Starting Server";
 }
 
 function stopServer(){
-    $output = shell_exec("sudo /etc/init.d/minecraft stop");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft stop");
 	echo "Stopping Server";
 }
 
 function updateServer(){
-    $output = shell_exec("sudo /etc/init.d/minecraft update");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft update");
 	echo "Updating Server";
 }
 
 function restartServer(){
-    $output = shell_exec("sudo /etc/init.d/minecraft restart");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft restart");
 	echo "Restarting Server";
 }
 
 function statusServer(){
-    $output = shell_exec("sudo /etc/init.d/minecraft status");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft status");
 	echo "Server Status";
 }
 
 function backupServer(){
-    $output = shell_exec("sudo /etc/init.d/minecraft backup");
+    $output = shell_exec("/usr/bin/sudo -u mcwebmin /etc/init.d/minecraft backup");
 	echo "Backing Up Server";
 }
 ?>
