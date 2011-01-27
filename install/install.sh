abc1362fc5354973c16e3550e048fc7a1df8d85c
Squeazer <miha.krajnc.mb@gmail.com>
2011-01-27 01:45:09 +0100

    Added c10t to install, fixed a bunch of other stuff.
    Apache still does not work properly since i have no idea how to install
    apache2-mpm-itk, or the will to figure it out at 2 am in the morning.
    
    Signed-off-by: Squeazer <miha.krajnc.mb@gmail.com>

--- install/install.sh
+++ install/install.sh
@@ -8,7 +8,7 @@ echo "##########################################"
 echo "#     This will install the latest       #"
 echo "#    version of the MCWebmin software    #"
 echo "##########################################"
-echo "#########|Installer version 0.7|##########"
+echo "#########|Installer version 0.7.1|########"
 echo "##########################################"
 echo
 echo
@@ -28,16 +28,24 @@ INSTALL_DIR="/usr/local/mcwebmin" #No last /
 #Import Distribution information file
 . /etc/*-release
 
+#Check if system is 64bit
+if [ $(getconf LONG_BIT) = 64 ]; then
+	BIT="x86_64"
+else
+	BIT="x86"
+fi
+
 #Detect linux Distro. If unsuccessful, ask for it.
-#Only works for Ubuntu / Debian.
+#Only works for Ubuntu :/
 if [ "$DISTRIB_ID" = "Debian" ] || [ "$DISTRIB_ID" = "Ubuntu" ]; then
 	echo "Detected $DISTRIB_ID."
-	DISTRO=$DISTRIB_ID
+	DISTRO="Debian"
 else
 	echo "Could not detect Linux Distribution, or distribution not supported"
 	echo "Are you running Debian based Linux with apt (Debian or Ubuntu) or RHEL based Linux with yum (Red Hat, CentOS, Fedora)?"
 		select DISTRO in Debian RHEL; do
 			echo "Selected $DISTRO"
+			break
 		done
 fi
 
@@ -61,8 +69,8 @@ echo "Would you like to update the system (recommended) [Y/n]?"
 echo "Would you like Minecraft to start when the computer starts? [Y/n]"
 	read AUTOSTARTMC
 
-echo "Would you like MCWebmin to start when the computer starts? [Y/n]"
-	read AUTOSTARTWEB
+#echo "Would you like MCWebmin to start when the computer starts? [Y/n]"
+#	read AUTOSTARTWEB
 
 echo "Would you like to download the newest version of minecraft_server.jar? [Y/n]"
 	read DOWNLOADMINECRAFT
@@ -75,7 +83,7 @@ OPTIONS="Apache2 Lighttpd[default] None"
 select opt in $OPTIONS; do
 	if [ "$opt" = "Apache2" ]; then
 		if [ "$DISTRO" = "Debian" ]; then
-			DEPENDENCIES="$DEPENDENCIES apache2 php5 php5-cli apache2-mpm-itk"
+			DEPENDENCIES="$DEPENDENCIES libapache2-mod-php5 php5-cli php5-common php5-cgi" #apache2-mpm-itk
 			WEBSERVER="apache"
 		elif [ "$DISTRO" = "RHEL" ]; then
 			DEPENDENCIES="$DEPENDENCIES httpd php php-cli"
@@ -84,7 +92,7 @@ select opt in $OPTIONS; do
 			break
 	elif [ "$opt" = "Lighttpd[default]" ] || [ "$opt" = "" ]; then
 		if [ "$DISTRO" = "Debian" ]; then
-			DEPENDENCIES="$DEPENDENCIES lighttpd php5-cli"
+			DEPENDENCIES="$DEPENDENCIES lighttpd php5-cli php5-cgi"
 			WEBSERVER="lighttpd"
 			break
 		elif [ "$DISTRO" = "RHEL" ]; then
@@ -97,6 +105,12 @@ select opt in $OPTIONS; do
 		break
 	fi
 done
+
+
+#Set multiplexer password
+echo "Set your multiplexer password (the one you will use to connect to this minecraft server)"
+read -s -p "Password: " MULTIPLEXER_PASS
+
 
 #Update the system if user wants to do so
 if [ "$UPDATESYSTEM" = "Y" ] || [ "$UPDATESYSTEM" = "" ] || [ "$UPDATESYSTEM" = "y" ]; then
@@ -134,6 +148,33 @@ echo "Moving MCWebmin to $INSTALL_DIR"
 	mv MCWebmin/ $INSTALL_DIR
 echo "Done"
 
+#Install c10t, latest version.
+echo "Installing c10t"
+#	wget -q http://toolchain.eu/minecraft/c10t/releases/CURRENT
+#	C10T_VERSION="cat CURRENT" #- not working
+#	rm CURRENT
+	
+	#Install 64 or 32 bit
+	wget -O c10t.tar.gz http://toolchain.eu/minecraft/c10t/releases/c10t-1.5-linux-$BIT.tar.gz
+	tar -xf c10t.tar.gz
+	mv c10t-*/ c10t
+	mv c10t/ $INSTALL_DIR/
+
+#Make multiplexer configuration
+#I am lazy
+echo "[java]" > mineremote.ini
+echo "heap_max = 1024M" >> mineremote.ini
+echo "gui = false" >> mineremote.ini
+echo "heap_min = 1024M" >> mineremote.ini
+echo "server = ./craftbukkit.jar" >> mineremote.ini
+echo "" >> mineremote.ini
+echo "[remote]" >> mineremote.ini
+echo "socktype = tcp" >> mineremote.ini
+echo "password = $MULTIPLEXER_PASS" >> mineremote.ini
+echo "listenaddr = 127.0.0.1" >> mineremote.ini
+#Move it to the right dir
+mv mineremote.ini $INSTALL_DIR/minecraft/mineremote.ini
+
 #If Lighttpd selected for install, configure it here.
 if [ $WEBSERVER = "lighttpd" ]; then
 	#Download configuration
@@ -167,9 +208,6 @@ if [ $WEBSERVER = "lighttpd" ]; then
 	echo "Copying Lighttpd configuration"
 		mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.bak
 		mv lighttpd.conf /etc/lighttpd/lighttpd.conf
-	echo "Making Lighttpd Folders"
-		mkdir /var/run/lighttpd/
-		mkdir /var/log/lighttpd/
 	echo "Changing Ownership of folders"
 		chown -R $USER:$USER /var/log/lighttpd/
 		chown -R $USER:$USER /var/run/lighttpd/
@@ -191,27 +229,40 @@ elif [ $WEBSERVER = "apache" ]; then
 			if [ "$APACHEPORT" = "" ]; then
 				APACHEPORT="80"
 			fi
+			
+			#Set virtual host file name
+			if [ -z "$DOMAIN" ]; then
+				VHOST_FILE="mcwebmin.vhost"
+			else
+				VHOST_FILE=$DOMAIN
+			fi
+			
+			#Add Listen directive
+			if [ "$APACHEPORT" != "80" ]; then
+				echo "Listen $APACHEPORT" > $VHOST_FILE
+			fi
 
-			echo "<VirtualHost *:$APACHEPORT>" > $DOMAIN
+			#Add config start
+			echo "<VirtualHost *:$APACHEPORT>" >> $VHOST_FILE
 
 			if [ "$DOMAIN" != "" ]; then				
-				echo "	ServerName $DOMAIN" >> $DOMAIN
+				echo "	ServerName $DOMAIN" >> $VHOST_FILE
 			fi
 
 			echo "Your email address (optional):"
 				read EMAILADDR
 					if [ ! $EMAILADDR = "" ]; then
-						echo "	ServerAdmin $EMAILADDR" >> $DOMAIN
+						echo "	ServerAdmin $EMAILADDR" >> $VHOST_FILE
 					fi
 
-			cat apache.vhost >> $DOMAIN
+			cat apache.vhost >> $VHOST_FILE
 			rm apache.vhost
 
 			#Move to virtual host to the apache sites-available dir
-			mv $DOMAIN /etc/apache2/sites-available/
+			mv $VHOST_FILE /etc/apache2/sites-available/
 
 			#Enable the VH
-			a2ensite $DOMAIN			
+			a2ensite $VHOST_FILE			
 
 			#Add selected user to the system
 			useradd -d $INSTALL_DIR $USER
